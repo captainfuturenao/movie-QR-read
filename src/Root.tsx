d806c8507dc170d5d008e647d3ceeb58548fdd37
Naoki <naoki.joy@gmail.com>
2026-02-07 00:31:41 +0900

    Deploy new video with native controls and mobile optimization

--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot: React.FC = () => {
             <Composition
                 id="MyComp"
                 component={MyComposition}
-                durationInFrames={450}
+                durationInFrames={900}
                 fps={30}
                 width={1920}
                 height={1080}
